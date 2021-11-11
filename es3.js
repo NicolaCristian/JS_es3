68956112da55ea4e33d908cc63ce1a274093fe0a
Nicola Cristian <nicola.cristian@osdbsesto>
2021-11-11 10:54:47 +0100

    semplification of the code

--- es3.js
+++ es3.js
@@ -1,45 +1,59 @@
 
-$("#btnpiu").click(function(){ 
-   
-   var num1=parseInt($("#num1").val());
-   var num2=parseInt($("#num2").val());
-   var ris=num1+num2;
+$("#btnpiu").click(function () {
+  calculation("+");
+});
+$("#btnmeno").click(function () {
+  calculation("-");
+});
+$("#btnper").click(function () {
+  calculation("*");
+});
+$("#btndiv").click(function () {
+  calculation("/");
+});
 
-   console.log(num1);
-   console.log(num1);
+var calculation = function (segno) {
+  var num1 = parseInt($("#num1").val());
+  var num2 = parseInt($("#num2").val());
 
-    ris=num1+num2;
-    var stringa=
-     "<tr><td>"+num1+"</td>"+
-     "<td style ='text-align:center'>"+"+"+"</td>+"+
-     "<td>"+num2+"</td>"+
-     "<td>"+ris+"</td></tr>";
-    $("#tabella").append(stringa);
+  console.log(num1);
+  console.log(num1);
 
-   });
+  switch (segno) {
+    case "+":
+      var ris = num1 + num2;
+      break;
 
+    case "-":
+      var ris = num1 - num2;
+      break;
 
+    case "*":
+      var ris = num1 * num2;
+      break;
 
-   $("#btnmeno").click(function(){ 
-   
-    var num1=parseInt($("#num1").val());
-    var num2=parseInt($("#num2").val());
-    var ris=num1+num2;
- 
-    console.log(num1);
-    console.log(num1);
- 
-     ris=num1-num2;
-     var stringa=
-      "<tr><td>"+num1+"</td>"+
-      "<td style ='text-align:center'>"+"-"+"</td>+"+
-      "<td>"+num2+"</td>"+
-      "<td>"+ris+"</td></tr>";
-     $("#tabella").append(stringa);
- 
-    });
+    case "/":
+      var ris = num1 / num2;
+      break;
+  }
+  var stringa =
+    "<tr><td>" +
+    num1 +
+    "</td>" +
+    "<td style ='text-align:center'>" +
+    segno +
+    "</td>+" +
+    "<td>" +
+    num2 +
+    "</td>" +
+    "<td>" +
+    ris +
+    "</td></tr>";
 
-    
+  $("#tabella").append(stringa);
+};
+
+/*
 $("#btnper").click(function(){ 
    
   var num1=parseInt($("#num1").val());
@@ -78,3 +92,4 @@ $("#btndiv").click(function(){
    $("#tabella").append(stringa);
 
   });
+*/
